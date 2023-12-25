577b7ba685d0616cfad02bb17068b6cfa0f2a1b8
Rus Price <rus@anolet.com>
2023-12-25 18:54:18 -0500

    My very first iOS app dev commit! :)

---
+++ idktest/idktest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  idktest
+//
+//  Created by Anderson Pierce on 12/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hey guys")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ idktest/idktest/idktestApp.swift
@@ -0,0 +1,17 @@
+//
+//  idktestApp.swift
+//  idktest
+//
+//  Created by Anderson Pierce on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct idktestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
